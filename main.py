cf35f8013bef1fa0d55467f1968ca380da13b316
Tanveer Najib <tanveernajib@gmail.com>
2023-10-21 11:42:51 +0100

    Add icons to inital list items

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 """
 edyka
 """
+import os
 import sys
 
 # pylint: disable=import-error
+import xbmc
+import xbmcaddon
 import xbmcgui
 import xbmcplugin
+import xbmcvfs
 # pylint: enable=import-error
 
 from resources.lib.extractor import search_movie, search_tv, get_links
@@ -18,6 +22,16 @@ else:
 
 URL = sys.argv[0]
 HANDLE = int(sys.argv[1])
+ADDON = xbmcaddon.Addon(id="plugin.video.edyka")
+
+# pylint: disable=bare-except
+try:
+    ADDON_PATH = xbmcvfs.translatePath(ADDON.getAddonInfo("path"))
+except:
+    ADDON_PATH = xbmc.translatePath(ADDON.getAddonInfo("path")).decode("utf-8")
+# pylint: enable=bare-except
+
+ICONS_DIR = os.path.join(ADDON_PATH, "resources", "images", "icons")
 
 def _build_url(**kwargs):
     """
@@ -38,6 +52,7 @@ def list_searchables():
         "title": "Movies",
         "mediatype": "video"
     })
+    movie_item.setArt({"icon": os.path.join(ICONS_DIR, "movie.png")})
     url = _build_url(mode="searchmovie")
     xbmcplugin.addDirectoryItem(HANDLE, url, movie_item, True)
 
@@ -46,6 +61,7 @@ def list_searchables():
         "title": "TV",
         "mediatype": "video"
     })
+    tv_item.setArt({"icon": os.path.join(ICONS_DIR, "tv.png")})
     url = _build_url(mode="searchtv")
     xbmcplugin.addDirectoryItem(HANDLE, url, tv_item, True)
 
